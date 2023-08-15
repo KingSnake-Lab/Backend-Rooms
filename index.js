e4f9b7121e8c37f3e36fd3ef10c1d0d255a460ac
ReplacedSpace17 <replacedspace17@gmail.com>
2023-08-15 00:38:18 -0600

    inicio de repo

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+
+// Resto de tu código Express aquí
+
+app.listen(3000, () => {
+  console.log('Servidor Express iniciado en el puerto 3000');
+});
