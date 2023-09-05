9b4459eeb84a1596cbd4904986376c8eac42e86a
ReplacedSpace17 <replacedspace17@gmail.com>
2023-09-05 05:46:54 -0600

    innovatect

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ const app = express();
 
 const { test } = require('./Functions/test');
 
+const host = '0.0.0.0'; // Escucha en todas las interfaces
 
 // Resto de tu código Express aquí
-app.post('/api/test', async (req, res) => {
+app.get('/api/test', async (req, res) => {
   //Obtener el body
   //const { email, password } = req.body;
   console.log("holaa");
-  test(req, res);
+
+
 });
 
+// Ruta para manejar la solicitud POST en '/miRuta'
+app.post('/miRuta', (req, res) => {
+  const textoRecibido = req.body; // Obtiene el texto del cuerpo de la solicitud POST
+  console.log('Texto recibido:', textoRecibido);
+  
+  // Responde a la solicitud con un mensaje de éxito
+  res.status(200).send('Texto recibido con éxito');
+});
 
-app.listen(3001, () => {
+app.listen(3000, host, () => {
  
-  console.log('Servidor Express iniciado en el puerto 3001');
+  console.log('Servidor Express iniciado en el puerto 3000');
 });
